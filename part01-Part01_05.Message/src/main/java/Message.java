53f8cc9945f889b84198f407585099de72abc421
Charley <52578804+charl3y@users.noreply.github.com>
2023-11-29 22:42:27 -0800

    Finished Message exercise

--- part01-Part01_05.Message/src/main/java/Message.java
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -7,7 +7,13 @@ public class Message {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write a message:");
-        // Write your program here
+
+        // Read the string written by the user, and assign it
+        // to program memory 
+        String message = scanner.nextLine();
+
+        // Print the message written by the user
+        System.out.println(message);
 
     }
 }
