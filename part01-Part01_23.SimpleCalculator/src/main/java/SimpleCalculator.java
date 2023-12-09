bf94458fcc5e8b9b31cff5339e236fa65715c27c
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-09 15:38:57 -0800

    Finished SimpleCalculator exercise

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -6,7 +6,33 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        //sum
+        int sum = first + second;
 
+        //difference
+        int difference = first - second;
+
+        //product
+        int product = first * second;
+
+        //quotient
+        double quotient = 1.0 * first / second; 
+
+        //print sum 
+        System.out.println(first + " " + "+" + " " + second + " " + "=" + " " + sum);
+
+        //print differnce
+        System.out.println(first + " " + "-" + " " + second + " " + "=" + " " + difference);
+
+        //print product
+        System.out.println(first + " " + "*" + " " + second + " " + "=" + " " + product);
+
+        //print quotient
+        System.out.println(first + " " + "/" + " " + second + " " + "=" + " " + quotient);
     }
 }
