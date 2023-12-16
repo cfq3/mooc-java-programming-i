e7fd5be339a86ae5cf6efadf9e02ae0c45e92066
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-16 12:27:15 -0800

    Finished AbsoluteValue exercise

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,19 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Declare variable and assign user input to it
+        int number = Integer.valueOf(scanner.nextLine());
+
+        // Operations and declaring the variable for result
+        int negative = number * -1;
+
+        // Number used in conditioal operations
+        if (number < 0) {
+            System.out.println(negative);
+        } else if (number > 0) {
+            System.out.println(number);
+        } else {
+            System.out.println(number);
+        }
     }
 }
