d383eaca1cf5665799c700d5f9d91c9f021a11ee
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-16 12:32:20 -0800

    Finished ComparingNumbers exercise

--- part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -6,5 +6,17 @@ public class ComparingNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Declare two variables and assign user input to it.
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        int secondNum = Integer.valueOf(scanner.nextLine());
+
+        // Use variables in condiitonal operations.
+        if (firstNum > secondNum) {
+            System.out.println(firstNum + " is greater than " + secondNum);
+        } else if (firstNum < secondNum) {
+            System.out.println(firstNum + " is smaller than " + secondNum);
+        } else {
+            System.out.println(firstNum + " is equal to " + secondNum);
+        }
     }
 }
