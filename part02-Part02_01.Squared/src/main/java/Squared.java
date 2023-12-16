90f88f4fd4a0c12e997ef29fa15540c69c820fb2
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-15 16:56:52 -0800

    Finished Squared exercise

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,14 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        // Get number from user
+        int number = Integer.valueOf(scanner.nextLine());
 
+        // Square operation
+        int square = number * number;
+
+        // Printing the result of the operation
+        System.out.println(square);
     }
 }
