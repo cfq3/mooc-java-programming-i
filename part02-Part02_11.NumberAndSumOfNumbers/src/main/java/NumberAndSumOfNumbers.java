c3e3e04d9f20ea6ca012174996fbfce918977325
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-19 23:31:48 -0800

    Finished NumberAndSumOfNumbers exercise

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -6,5 +6,37 @@ public class NumberAndSumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // initialize variables for count and sum
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+            // for entering number
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            // break loop if input is 0
+            if (number == 0) {
+                break;
+            }
+
+            // loops to add to count
+            if (number > 0) {
+                count = count + 1;
+            }
+            if (number < 0) {
+                count = count + 1;
+            }
+
+            // loops to add sum
+            if (number < 0) {
+                sum = number + sum;
+            }
+            if (number > 0) {
+                sum = number + sum;
+            }
+        }
+        System.out.println("Number of numbers: " + count);
+        System.out.println("Sum of the numbers: " + sum);
     }
 }
