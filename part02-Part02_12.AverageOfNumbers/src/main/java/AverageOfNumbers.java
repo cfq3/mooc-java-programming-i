4d8ad2f49a3918589997aee33e532bf6866635e4
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-20 16:26:33 -0800

    Finished AverageOfNumbers exercise

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,41 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // initialize variables for counting, sum and average
+        int count = 0;
+        double sum = 0;
+        double average = 0;
+
+        while (true) {
+            // prompts user
+            // user value is assigned to number
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            // if input is '0' break loop
+            if (number == 0) {
+                break;
+            }
+
+            // loops for adding count. i
+            // increment count by 1 for each positive and negative number
+            if (number > 0) {
+                count = count + 1;
+            }
+            if (number < 0) {
+                count = count + 1;
+            }
+            // loops for sum
+            // sum initialzed as zero. add number value to sum for each positive and negative number
+            if (number > 0) {
+                sum = number + sum;
+            }
+            if (number < 0) {
+                sum = number + sum;
+            }
+        }
+        // calculate and print out average
+        average = (sum / count) * 1.0;
+        System.out.println("Average of the numbers: " + average);
     }
 }
