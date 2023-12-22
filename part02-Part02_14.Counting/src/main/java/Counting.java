ae4a69119b306c4d929843d53155af41c5ed4e20
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-21 21:50:57 -0800

    Finished Counting exercise

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -6,5 +6,10 @@ public class Counting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = Integer.valueOf(scanner.nextInt());
+        number = number + 1;
+        for (int i = 0; i < number; i++) {
+            System.out.println(i);
+        }
     }
 }
