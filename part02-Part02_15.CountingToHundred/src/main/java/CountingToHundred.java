754ba32cf3d8723910a5785acc36a41b7fc9bbf3
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-22 18:00:55 -0800

    Finished CountingToHundred exercise

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,9 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = Integer.valueOf(scanner.nextLine());
+        for (int i = number; i < 101; i++) {
+            System.out.println(i);
+        }
     }
 }
