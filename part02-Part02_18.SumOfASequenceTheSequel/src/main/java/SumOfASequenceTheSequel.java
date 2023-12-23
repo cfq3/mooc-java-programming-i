7553952fb180ff9bc52115a42a99e0109db18880
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-23 15:21:51 -0800

    Finished SumOfASequenceTheSequel exercise

--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -6,5 +6,22 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // initialize sum
+        int sum = 0;
+        
+        // get user input to use in loop
+        System.out.println("First number?");
+        int firstNum = Integer.valueOf(scanner.nextInt());
+        System.out.println("Last number?");
+        int lastNum = Integer.valueOf(scanner.nextInt());
+
+        // loop starts at firstNum, ends at lastNum and adds all numbers starting from firstNum -> lastNum
+        int i = firstNum;
+        while (i <= lastNum) {
+            sum = sum + i;
+            i++;
+        }
+        // print out sum
+        System.out.println("The sum is: " + sum);
     }
 }
