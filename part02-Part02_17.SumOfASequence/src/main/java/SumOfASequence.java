a03b916fa70394e798d434f34b8b92d1c41891e1
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-23 15:06:44 -0800

    Finished SumOfASequence exercise

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -6,5 +6,17 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // initialze sum
+        int sum = 0;
+        // ask for user input
+        System.out.println("Last number?");
+        int n = Integer.valueOf(scanner.nextLine());
+
+        int i = 0;
+        while (i <= n) {
+            sum = i + sum;
+            i++;
+        }
+        System.out.println("The sum is: " + sum);
     }
 }
