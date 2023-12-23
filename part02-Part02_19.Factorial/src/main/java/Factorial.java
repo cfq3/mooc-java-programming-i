318678ee39b024335317e54beba2f58242d1c948
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-23 15:54:29 -0800

    Finished Factorial exercise

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,19 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        // get number from user
+        System.out.println("Give a number: ");
+        int num = Integer.valueOf(scanner.nextInt());
 
+         // initialize factorial variable (start at 1)
+         // start i at fact; i ends at num
+        int fact = 1;
+        for (int i = fact; i <= num; i++) {
+            fact = fact * i;
+        }
+
+        // print out factorial
+        System.out.println("Factorial: " + fact);
     }
 }
