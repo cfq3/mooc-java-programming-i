6614ac8606d3c112a2130998f43df0d71a50237c
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-27 15:12:07 -0800

    Finished FromOneToParameter exercise

--- part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -3,7 +3,15 @@
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        printUntilNumber(5);
     }
 
+    public static void printUntilNumber(int number) {
+        // i used to count # of times the loop has been executed. value set to 1
+        // loop condition i <= number
+        // i++ (increase value of variable i = i + 1)
+        for (int i = 1; i <= number; i++) {
+            System.out.print(i);
+        }
+    }
 }
