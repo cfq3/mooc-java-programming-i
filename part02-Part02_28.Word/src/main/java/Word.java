9c62b95a2a77f0e7706ef68364f4de52bf1b9e72
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-30 14:54:13 -0800

    Finished Word exercise

--- part02-Part02_28.Word/src/main/java/Word.java
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -6,6 +6,12 @@ public class Word {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        word();
     }
 
+    public static String word() {
+        String greeting = "hello";
+
+        return greeting;
+    }
 }
