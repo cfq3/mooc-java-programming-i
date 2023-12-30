cb2c35d253cfb246f9a30ffc0a163d2f1b663522
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-29 17:26:52 -0800

    Finished DivisibleByThree exercise

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,7 +5,18 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        divisibleByThreeInRange(2, 10);
+    }
 
+    // takes beginning and end as parameters
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        // for number i in range 'int beginning' to 'int end' print if divisible by 3
+        // increases from value of 'int beginning' to value of 'int end'
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 
 }
