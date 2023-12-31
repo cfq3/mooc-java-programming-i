061d501ce660f5e04a56dc0c22841f0b41dc5fd7
Charley <52578804+charl3y@users.noreply.github.com>
2023-12-31 12:51:29 -0800

    Finished Smallest exercise

--- part02-Part02_30.Smallest/src/main/java/Smallest.java
+++ part02-Part02_30.Smallest/src/main/java/Smallest.java
@@ -5,7 +5,14 @@ public class Smallest {
         // Write some code here
         // don't print anything inside this method
         // there must be a return in the end of the method
-        return -1;
+        int result = 0;
+        if (number1 < number2) {
+            result = number1;
+        }
+        if (number1 > number2) {
+            result = number2;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
