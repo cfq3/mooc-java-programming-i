7484d3404e17f22e4d48bdf8bd38d192b453e9f4
Charley <52578804+charl3y@users.noreply.github.com>
2024-01-03 06:44:41 -0800

    Finished StarSign exercise

--- part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -20,22 +20,30 @@ public class StarSign {
 
     public static void printStars(int number) {
         // first part of the exercise
-        System.out.println("*");
+        for(int i = 1; i <= number; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
     }
 
     public static void printSquare(int size) {
         // second part of the exercise
-        System.out.println("****");
-        System.out.println("****");
-        System.out.println("****");
-        System.out.println("****");
+        for(int i = 1; i <= size; i++) {
+            printStars(size);
+        }
     }
 
     public static void printRectangle(int width, int height) {
         // third part of the exercise
+        for(int i = 1; i <= height; i++) {
+            printStars(width);
+        }
     }
 
     public static void printTriangle(int size) {
         // fourth part of the exercise
+        for(int i = 1; i <= size; i++) {
+            printStars(i);
+        }
     }
 }
