e1e20392a6e9c91488b09245174e5026be318143
Charley <52578804+charl3y@users.noreply.github.com>
2024-05-18 15:58:15 -0700

    Finished ArrayPrinter exercise

--- part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
+++ part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -9,5 +9,14 @@ public class ArrayPrinter {
 
     public static void printNeatly(int[] array) {
         // Write some code in here
+        int index = 0; // initialize counter
+        while (index < array.length) { // loop through until end of array
+            int number = array[index]; // get current element from the array
+            System.out.print(number); // print current element
+            if (index < array.length - 1) { // check if the current element is not the last element
+                System.out.print(", "); // print comma and space after the current element
+            }
+            index = index + 1; // increment counter
+        }
     }
 }
