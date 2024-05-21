01bef8db1503fc3c91065fe1ce7b1a83629dec81
Charley <52578804+charl3y@users.noreply.github.com>
2024-05-20 18:56:06 -0700

    Finished LineByLine exercise

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,17 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while(true) {
+            String userInput = scanner.nextLine();
 
+            String[] splitStringArray = userInput.split(" ");
+
+            for (String e : splitStringArray) {
+                System.out.println(e);
+            }
+            if (userInput.equals("")) {
+                break;
+            }
+        }
     }
 }
