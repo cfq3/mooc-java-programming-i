cd8605cb719940d61e5d81fd9151de2508db6cbb
Charley <52578804+charl3y@users.noreply.github.com>
2024-05-21 22:44:04 -0700

    Finished AVClub exercise

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,24 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Loop to read user input until an empty line is entered
+        while (true) {
+            String userInput = scanner.nextLine(); // Get user input
 
+            // Check if the user input is an empty line
+            if (userInput.equals("")) { // If the userInput is an empyty string
+                break; // Break the loop
+            }
+
+            // Split the user input into an array of words based on spaces
+            String[] splitStringArray = userInput.split(" "); // Split userInput
+
+            // Iterate over each word in the splitStringArray
+            for (String word : splitStringArray) { // For each word in the splitStringArray
+                if (word.contains("av")) { // Check if the word contains "av"
+                    System.out.println(word); // Print the word to the console
+                }
+            }
+        }
     }
 }
