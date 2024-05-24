66a645bb1cf60b6e7ae8925ed8b2cf4dc3d1b082
Charley <52578804+charl3y@users.noreply.github.com>
2024-05-23 19:05:13 -0700

    Finished FirstWords exercise

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -6,6 +6,23 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Reads user input until an empty line is entered
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.equals("")) {
+                break;
+            }
+
+            // For each non empty line in program
+            // Spit the string by spaces
+            // Print the first part of the string
+            String[] pieces = input.split(" ");
+
+            // Print the first part of the string if it exists
+            if (pieces.length > 0) {
+                System.out.println(pieces[0]);
+            }
+        }
     }
 }
