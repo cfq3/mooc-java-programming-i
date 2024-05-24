1dfede1f5ab7b8bec16c850028956438093ab92e
Charley <52578804+charl3y@users.noreply.github.com>
2024-05-24 00:34:55 -0700

    Finished LastWords exercise

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -6,6 +6,23 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Reads user input until an empty line is entered
+        while (true) {
+            String input = scanner.nextLine();
 
+            if (input.equals("")) {
+                break;
+            }
+
+            // For each non-empty line, the program splits the string by spaces 
+            // and prints the last part of the string
+
+            String[] parts = input.split(" "); // splits input into array of substrings, using "" to separate & store in parts array
+            if (parts.length > 0) {
+                // Retrieve last element of the array
+                String lastElement = parts[parts.length - 1];
+                System.out.println(lastElement);
+            }
+        }
     }
 }
