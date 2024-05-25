5bdc4e38e93fc285fd55fee3d07bf5e261d364d6
Charley <52578804+charl3y@users.noreply.github.com>
2024-05-24 23:21:28 -0700

    Finished "AgeOfTheOldest" exercise

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,17 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int maxAge = 0; // Initialize max age to 0
+        String input = scanner.nextLine();
 
-
+        while (!(input.equals(""))) {
+            String[] part = input.split(","); // Split the input string into an array of strings and use comma as delimiter
+            if (maxAge < Integer.valueOf(part[1])) { // if the current age is greater than 'maxAge'
+                maxAge = Integer.valueOf(part[1]);  // update maxAge if the current age is greater
+            }
+            input = scanner.nextLine(); // read the next line of input
+        } 
+        System.out.println("Age of the oldest: " + maxAge); // print the age of the oldest person
     }
 }
+
