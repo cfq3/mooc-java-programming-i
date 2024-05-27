f9ce88e193287eae19005debaeb6222ec32f67c1
charley <q1charley@gmail.com>
2024-05-27 01:27:41 -0700

    Completed exercise

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,46 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Initialize variables
+        int totalBirthYears = 0;
+        int count = 0;
+        String longestName = "";
+        int maxLength = 0;
 
+        while (true) {
+            String input = scanner.nextLine(); // Get input from user
+
+            // End loop if user enters an empty line
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] data = input.split(","); // Split input string by comma
+            int birthyear = Integer.valueOf(data[1]); // Convert birthyear into type int
+            String name = data[0];
+
+            // Calculate longest name
+            int length = name.length();
+            if (length > maxLength) {
+                maxLength = length;
+                longestName = name;
+            }
+
+            // Add birthyear to total and increment count
+            totalBirthYears += birthyear;
+            count++;
+
+        }
+
+        // Calculate average birth year
+        int avgBirthyear;
+        if (count > 0) {
+            avgBirthyear = totalBirthYears / count;
+        } else {
+            avgBirthyear = 0;
+        }
+
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + avgBirthyear);
     }
 }
