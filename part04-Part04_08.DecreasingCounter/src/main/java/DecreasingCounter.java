9d5b05ab862222b8b9d6ad36acfd0527dce6d2e4
Charley <52578804+charl3y@users.noreply.github.com>
2024-06-02 20:41:50 -0700

    Finished DecreasingCounter exercise

--- part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
+++ part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
@@ -15,8 +15,15 @@ public class DecreasingCounter {
     public void decrement() {
         // write the method implementation here
         // the aim is to decrement the value of the counter by one
-        this.value = this.value - 1;
+        if (this.value == 0) {
+            this.value = 0; 
+        } else {
+            this.value = this.value - 1;
+        }
     }
 
     // the other methods go here
+    public void reset() {
+        this.value = 0; 
+    }
 }
