bcb85a40f1d31fd240ad82bda35b68114e3d747d
Charley <52578804+charl3y@users.noreply.github.com>
2024-06-07 11:14:20 -0700

    Finished part 1

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,16 @@
 
 public class Statistics {
+    private int count;
+
+    public Statistics() {
+        this.count = 0;
+    }
+
+    public void addNumber(int number) {
+        this.count++;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
 }
