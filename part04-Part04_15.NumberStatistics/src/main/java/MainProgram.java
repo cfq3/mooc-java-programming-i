e3a9e0bec3575292929b8acef51a939977f9f4dc
Charley <52578804+charl3y@users.noreply.github.com>
2024-06-07 20:20:27 -0700

    Removed comments

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,13 +5,7 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
         Statistics statistics = new Statistics();
         statistics.addNumber(3);
         statistics.addNumber(5);
