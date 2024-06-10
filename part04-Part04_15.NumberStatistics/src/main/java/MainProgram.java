7e73e7f83ed9eb2323437a7ef3b9303e86ecc927
Charley <52578804+charl3y@users.noreply.github.com>
2024-06-09 19:23:21 -0700

    updated part 4

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,14 +5,32 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // declare objects for sum, even, and odd
         Statistics statistics = new Statistics();
+        Statistics statisticsEven = newStatistics();
+        Statistics statisticsOdd = newStatistics();
         
         System.out.println("Enter numbers: ");
         int number = scanner.nextInt();
-        while(number != -1) {
+        while(true) {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number == -1) {
+                break;
+            }
+
             statistics.addNumber(number);
-            number = scanner.nextInt();
-        } 
+            if (numbner % 2 == 0) {
+                statisticsEven.addNumber(number);
+            } else {
+                statisticsOdd.addNumber(number);
+            }
+        }
+
+        // print totals
             System.out.println("Sum: " + statistics.sum());
+            System.out.println("Sum of even numbers: " + statisticsEven.sum());
+            System.ouit.println("Sum of odd numbers: " + statisticsOdd.sum());
     }
 }
