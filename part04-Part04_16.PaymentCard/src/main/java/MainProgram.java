e931b572808224744cd45e350e724cb789beb8df
Charley <52578804+charl3y@users.noreply.github.com>
2024-06-12 03:58:31 -0700

    Finished Part 1

--- part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
+++ part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
@@ -2,10 +2,9 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        // Scanner scanner = new Scanner(System.in);
-        
-        // Here you can write code to test that PaymentCard works as intended
-        // be sure to erase the extra code for the last part of the exercise!
+        //Scanner scanner = new Scanner(System.in);
+        PaymentCard card = new PaymentCard(50);
+        System.out.println(card);
 
     }
 }
