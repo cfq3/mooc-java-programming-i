8116d785723740cd76cc00657d4a50be7db5ac21
Charley <52578804+charl3y@users.noreply.github.com>
2024-06-13 12:08:02 -0700

    Finished Items exercise

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -10,5 +10,21 @@ public class Items {
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            // Get user input
+            System.out.println("Name: ");
+            String itemName = scanner.nextLine();
+
+            // Break while loop if empty
+            if (itemName.isEmpty()) {
+                break;
+            }
+
+            // Add to items list
+            items.add(new Item(itemName));
+        }
+        for (Item item: items) {
+            System.out.println(item);
+        }
     }
 }
