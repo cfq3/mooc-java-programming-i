bb15d016d78ffaed4095c7f0097f34d60106709d
Charley <52578804+charl3y@users.noreply.github.com>
2024-06-14 15:50:33 -0700

    Finished Personal information exercise

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,29 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // Get user's first name 
+            System.out.println("First name: ");
+            String userFirstName = scanner.nextLine();
 
+            // If user input's nothing, break loop
+            if(userFirstName.isEmpty()) {
+                break;
+            }
+
+            // Get user's last name
+            System.out.println("Last name: ");
+            String userLastName = scanner.nextLine();
+
+            // Get id number
+            System.out.println("Identification number: ");
+            String idNumber = scanner.nextLine();
+
+            infoCollection.add(new PersonalInformation(userFirstName, userLastName,idNumber));
+        }
+
+        for (PersonalInformation e : infoCollection) {
+            System.out.println(e.getFirstName() + " " + e.getLastName());
+        }
     }
 }
