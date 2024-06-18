4317f69f6cca5d45ba31f2f09e2d5a5af2ceeb78
Charley <52578804+charl3y@users.noreply.github.com>
2024-06-18 13:10:54 -0700

    Finished Books exercise

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,42 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
+        ArrayList<Book> books = new ArrayList<>();
 
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Title: ");
+            String inputTitle = sc.nextLine();
+            if (inputTitle.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Pages: ");
+            int inputPages = Integer.valueOf(sc.nextInt());
+
+            System.out.println("Publication Year: ");
+            int inputPublicationYear = Integer.valueOf(sc.nextInt());
+
+            books.add(new Book(inputTitle, inputPages, inputPublicationYear));
+        }
+
+        System.out.println("What information will be printed?");
+
+        String infoToBePrinted = sc.nextLine();
+
+        if (infoToBePrinted.toLowerCase().equals("everything")) {
+            for (Book element : books) {
+                System.out.print(element);
+            }
+        }
+            if (infoToBePrinted.toLowerCase().equals("name")) {
+                for (Book element : books) {
+                    System.out.println(element.getTitle());
+                }
+            } else {
+                return;
+            }
+        }
     }
 }
