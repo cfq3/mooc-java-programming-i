e0a30ea72427275f8f85e11cfbecf8a685b5ecdb
Charley <52578804+charl3y@users.noreply.github.com>
2024-06-21 02:10:21 -0700

    Finished PrintingAFile exercise

--- part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 public class PrintingAFile {
 
     public static void main(String[] args) {
+        // create a scanner for reading the file
+       try (Scanner scanner = new Scanner(Paths.get("data.txt"))) {
 
+        // read the file until all lines have been read
+        while (scanner.hasNextLine()) {
+            // read one line
+            String row = scanner.nextLine();
+            // print the line that is read
+            System.out.println(row);
+        }
+       } catch (Exception e) {
+        System.out.println("Error: " + e.getMessage());
+       }
     }
 }
