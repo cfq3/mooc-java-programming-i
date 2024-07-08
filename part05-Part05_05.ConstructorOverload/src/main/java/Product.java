accdbc8953b489a27ab190b078495e5baf8a27c6
Charley <52578804+charl3y@users.noreply.github.com>
2024-07-08 02:33:34 -0700

    Finished Constructor Overload exercise

--- part05-Part05_05.ConstructorOverload/src/main/java/Product.java
+++ part05-Part05_05.ConstructorOverload/src/main/java/Product.java
@@ -5,12 +5,29 @@ public class Product {
     private String location;
     private int weight;
 
+    public Product(String name) {
+        this.name = name;
+        this.location = "shelf";
+        this.weight = 1;
+    }
+
+    public Product(String name, String location) {
+        this.name = name;
+        this.location = location;
+        this.weight = 1;
+    }
+
+    public Product(String name, int weight) {
+        this.name = name;
+        this.location = "shelf";
+        this.weight = weight;
+    }
+
     public Product(String name, String location, int weight) {
         this.name = name;
         this.location = location;
         this.weight = weight;
     }
-
     
     public String getName() {
         return name;
