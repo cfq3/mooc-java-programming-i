59d5fb5c9e66f22ae8424b939f42f4a81f074252
Charley <52578804+charl3y@users.noreply.github.com>
2024-07-21 20:17:53 -0700

    Finished part 1

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -4,7 +4,7 @@ public class HealthStation {
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        return person.getWeight();
     }
 
 }
