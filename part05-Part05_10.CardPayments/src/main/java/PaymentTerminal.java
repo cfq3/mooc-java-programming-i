51b449861448a3a939760d8a4fd28e75da1816c5
Charley <52578804+charl3y@users.noreply.github.com>
2024-07-27 03:50:14 -0700

    Finished part 2

--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -9,19 +9,32 @@ public class PaymentTerminal {
 
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
         // increase the amount of cash by the price of an affordable mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        double change = payment;
+        if (payment >= 2.50) {
+            affordableMeals += 1;
+            this.money += 2.50;
+            change = change - 2.50;
+        }
+        return change;
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+        double change = payment;
+        if (payment >= 4.30) {
+            heartyMeals += 1;
+            this.money += 4.30;
+            change = change - 4.30;
+        }
         return -1;
     }
 
